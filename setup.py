d072149140b10f4350a4ff06f79578f332d915de
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2018-09-21 11:49:50 +0100

    add setup.py in order to make the project installable

---
+++ setup.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+from setuptools import setup
+from setuptools import find_packages
+
+BASE_PATH = Path(__file__).parent
+
+
+def read(path: Path):
+    return path.read_text("utf-8")
+
+
+setup(
+    name="smbbackend",
+    version=read(BASE_PATH / "VERSION"),
+    description="Backend calculations for smb",
+    long_description=read(BASE_PATH / "README.md"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/geosolutions-it/smb-backend",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "boto3==1.9.0",
+        "psycopg2-binary==2.7.5",
+        "pytz==2018.5",
+        "zappa==0.46.2",
+    ]
+)
